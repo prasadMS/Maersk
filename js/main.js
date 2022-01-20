f088477725855215aff0f473f97caf34fb5a3f86
prasadMS <msprasadmca.prasad@gmail.com>
2022-01-20 16:10:35 +0530

    code added
    code added

--- js/main.js
+++ js/main.js
@@ -1,1 +1,28 @@
-js file added
+function shuffleAndSortNumbers(action) {
+    let arrayOfNumbers = [];
+
+    while(arrayOfNumbers.length < 9){
+        let num = Math.floor(Math.random() * 9) + 1;
+        if(arrayOfNumbers.indexOf(num) === -1) arrayOfNumbers.push(num);
+    }
+
+    if(action && action.toUpperCase() === 'SORT') {
+        arrayOfNumbers.sort();
+    }
+
+    renderHtml(arrayOfNumbers);
+}
+
+function renderHtml(data) {
+    var ul = document.getElementById("populateNumbers");
+    let liElements = '';
+
+    for(let i=0; i<data.length; i++) {  
+        let liClass = "li_"+ (i+1);              
+        liElements = liElements + "<li class='"+liClass+"' >" + data[i] + "</li>";              
+    }
+    
+    ul.innerHTML = liElements;
+}
+
+shuffleAndSortNumbers('shuffle');
